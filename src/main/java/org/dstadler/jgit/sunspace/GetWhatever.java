63b61b3346f79ae0ee8a0c0a2ba069f95dc0cfd7
QingYiSun <nju15sc@gmail.com>
2020-11-17 19:12:49 +0800

    most functions integrated

--- src/main/java/org/dstadler/jgit/sunspace/GetWhatever.java
+++ src/main/java/org/dstadler/jgit/sunspace/GetWhatever.java
@@ -8,23 +8,26 @@ import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.util.io.DisabledOutputStream;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class GetWhatever {
 
-    private static final String REMOTE_URL = "https://github.com/spring-projects/spring-boot.git";
+    private static final String REMOTE_URL = "https://github.com/spring-projects/spring-data-gemfire.git";
 
     private static final String path_head = "/Users/abnerallen/IdeaProjects/SunGit/jgit-cookbook/src/test";
-    private static final String path_rear = "/boot";
+    private static final String path_rear = "/gemfire";
 
     public static void main(String[] args) throws IOException, GitAPIException{
 
@@ -43,6 +46,8 @@ public class GetWhatever {
 
             int counter = 0; // count the number of commits in one repository
             ArrayList<ObjectId> allCommitIDs = new ArrayList<>(); // store the ID of each commit
+            ArrayList<String> CommitMSGs = new ArrayList<>(); // store all the commits' messages
+            ArrayList<RevCommit> allCommits = new ArrayList<>(); // store all the commits
 
             // get all the commits
             try(RevWalk revWalk = new RevWalk(git.getRepository())){
@@ -52,6 +57,8 @@ public class GetWhatever {
                 }
 
                 for(RevCommit commit: revWalk){
+                    allCommits.add(commit);
+                    CommitMSGs.add(commit.getFullMessage());
                     allCommitIDs.add(commit.getId());
                     counter++;
                 }
@@ -60,6 +67,10 @@ public class GetWhatever {
 
             // get all the diff entries
             for(int i = allCommitIDs.size() - 1; i >= 1; i--){
+            //    System.out.println("Commit Info: " + allCommits.get(i).getName() + "  " + allCommits.get(i).getFullMessage());
+            //    System.out.println("Commit Id: " + allCommitIDs.get(i).getName());
+            //    System.out.println("Commit MSG: " + CommitMSGs.get(i));
+
                 ObjectReader reader = git.getRepository().newObjectReader();
 
                 CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
@@ -76,16 +87,42 @@ public class GetWhatever {
 
                 for(DiffEntry entry: entries){
                     System.out.println(entry.getChangeType() + "  " + entry.getOldPath() + "  " + entry.getNewPath());
-                }
-            }
+                    if(entry.getChangeType().equals(DiffEntry.ChangeType.MODIFY)) {
+                        String tmpNew = readFile(git.getRepository(), allCommits.get(i), entry.getNewPath());
+                        System.out.println("new: " + tmpNew);
+                        String tmpOld = readFile(git.getRepository(), allCommits.get(i), entry.getOldPath());
+                        System.out.println("old: " + tmpOld);
+                    }
+                    //TODO: identify NewPath & OldPath or former(i) & latter(i - 1)
+                    //TODO: write all info into file defined by myself
+                    /*
+                    else if(entry.getChangeType().equals(DiffEntry.ChangeType.ADD)){
 
-            //TODO: extract files' names
-            //TODO: get the content of files
+                    }
+                    else if(entry.getChangeType().equals(DiffEntry.ChangeType.DELETE)){
+
+                    }
+                    */
+                }
+
+            }
 
         }
 
         FileUtils.deleteDirectory(localPath);
 
+    }
+
+    private static String readFile(Repository repo, RevCommit commit, String filePath) throws IOException{
+        try(TreeWalk walk = TreeWalk.forPath(repo, filePath, commit.getTree())){
+            if(null != walk){
+                byte[] bytes = repo.open(walk.getObjectId(0)).getBytes();
+                return new String(bytes, StandardCharsets.UTF_8);
+            }
+            else{
+                throw new IllegalArgumentException("No path found.");
+            }
+        }
     }
 
 }
